6f530b556b47bb1cfbdc6b8f240cea8cfc50a70a
Dylan Ray <ddray@email.unc.edu>
2022-07-27 21:41:01 -0400

    made compatible with multiple subdirs

--- src/data/relate.py
+++ src/data/relate.py
@@ -42,76 +42,79 @@ def parse_args():
 def main():
     args = parse_args()
     
+    idirs = [u for u in glob.glob(os.path.join(args.idir, '*')) if not '.' in u]
     
-    ### get haplotype and sample files
-    
-    logging.info('uncompressing data...')
-    ifiles = [os.path.join(args.idir, u) for u in os.listdir(args.idir) if (('.ms' in u) or ('.msOut' in u))]
-    for ifile in ifiles:
-        if '.gz' in ifile:
-            os.system('gzip -d {}'.format(ifile))
-    
-    rcmd = 'Rscript src/data/ms2haps.R {0} {1} {2}'
-    relate_cmd = 'relate/bin/Relate --mode All -m {0} -N {1} --haps {2} --sample {3} --map {4} --output {5}'
-    
-    for ifile in ifiles:
-        ifile = ifile.replace('.gz', '')
+    for idir in idirs:
+        logging.info('working on {}...'.format(idir))
         
-        logging.info('working on {}...'.format(ifile))
-        logging.info('converting to haps / sample files via Rscript...')
-        cmd_ = rcmd.format(ifile, ifile.split('.')[0], int(args.L))
-        os.system(cmd_)
+        ### get haplotype and sample files
+        logging.info('uncompressing data...')
+        ifiles = [os.path.join(idir, u) for u in os.listdir(idir) if (('.ms' in u) or ('.msOut' in u))]
+        for ifile in ifiles:
+            if '.gz' in ifile:
+                os.system('gzip -d {}'.format(ifile))
         
-        # read the ms file for the mutation rate and number of sites
-        msf = open(ifile, 'r')
-        lines = msf.readlines()
-
-        l = lines[3].replace('//', '').replace('*', '').split('\t')
+        rcmd = 'Rscript src/data/ms2haps.R {0} {1} {2}'
+        relate_cmd = 'relate/bin/Relate --mode All -m {0} -N {1} --haps {2} --sample {3} --map {4} --output {5}'
         
-        L = float(args.L)  
-        r = float(args.r)
-        mu = float(args.mu)
-        
-        n_sites = int(lines[4].split(':')[-1].replace('\n',''))
-        
-        ofile = open(ifile.split('.')[0] + '.map', 'w')
-        ofile.write('pos COMBINED_rate Genetic_Map\n')
-        ofile.write('0 {} 0\n'.format(r * L))
-        ofile.write('{0} {1} {2}\n'.format(L, r * L, r * 10**8))
-        ofile.close()
-        
-        ofile = open(ifile.split('.')[0] + '.poplabels', 'w')
-        ofile.write('sample population group sex\n')
-        for k in range(1, 26):
-            ofile.write('UNR{} POP POP 1\n'.format(k))
-        ofile.close()
-        
-        #ofile = open(ifile.split('.')[0] + '.relate.anc', 'w')
-        #ofile.write('>anc\n')
-        #ofile.write(sum(['0' for k in range(n_sites)]) + '\n')
-        #ofile.close()
-        
-        map_file = ifile.split('.')[0] + '.map'
-        samples = sorted(glob.glob(os.path.join(args.idir, '*.sample')))
-        haps = sorted(glob.glob(os.path.join(args.idir, '*.haps')))
-        
-        for ix in range(len(samples)):
-            cmd_ = relate_cmd.format(mu, L, haps[ix], 
-                                     samples[ix], map_file, 
-                                     haps[ix].split('/')[-1].split('.')[0])
+        for ifile in ifiles:
+            ifile = ifile.replace('.gz', '')
+            
+            logging.info('working on {}...'.format(ifile))
+            logging.info('converting to haps / sample files via Rscript...')
+            cmd_ = rcmd.format(ifile, ifile.split('.')[0], int(args.L))
             os.system(cmd_)
-        
-            os.system('mv {0}* {1}'.format(haps[ix].split('/')[-1].split('.')[0], args.odir))
-        
-        os.system('rm -rf {}'.format(os.path.join(args.idir, '*.sample')))
-        os.system('rm -rf {}'.format(os.path.join(args.idir, '*.haps')))
-        
-    # compress back
-    logging.info('compressing back...')
-    ifiles = [os.path.join(args.idir, u) for u in os.listdir(args.idir) if (('.ms' in u) or ('.msOut' in u))]
-    for ifile in ifiles:
-        if '.gz' not in ifile:
-            os.system('gzip {}'.format(ifile))
+            
+            # read the ms file for the mutation rate and number of sites
+            msf = open(ifile, 'r')
+            lines = msf.readlines()
+    
+            l = lines[3].replace('//', '').replace('*', '').split('\t')
+            
+            L = float(args.L)  
+            r = float(args.r)
+            mu = float(args.mu)
+            
+            n_sites = int(lines[4].split(':')[-1].replace('\n',''))
+            
+            ofile = open(ifile.split('.')[0] + '.map', 'w')
+            ofile.write('pos COMBINED_rate Genetic_Map\n')
+            ofile.write('0 {} 0\n'.format(r * L))
+            ofile.write('{0} {1} {2}\n'.format(L, r * L, r * 10**8))
+            ofile.close()
+            
+            ofile = open(ifile.split('.')[0] + '.poplabels', 'w')
+            ofile.write('sample population group sex\n')
+            for k in range(1, 26):
+                ofile.write('UNR{} POP POP 1\n'.format(k))
+            ofile.close()
+            
+            #ofile = open(ifile.split('.')[0] + '.relate.anc', 'w')
+            #ofile.write('>anc\n')
+            #ofile.write(sum(['0' for k in range(n_sites)]) + '\n')
+            #ofile.close()
+            
+            map_file = ifile.split('.')[0] + '.map'
+            samples = sorted(glob.glob(os.path.join(idir, '*.sample')))
+            haps = sorted(glob.glob(os.path.join(idir, '*.haps')))
+            
+            for ix in range(len(samples)):
+                cmd_ = relate_cmd.format(mu, L, haps[ix], 
+                                         samples[ix], map_file, 
+                                         haps[ix].split('/')[-1].split('.')[0])
+                os.system(cmd_)
+            
+                os.system('mv {0}* {1}'.format(haps[ix].split('/')[-1].split('.')[0], args.odir))
+            
+            os.system('rm -rf {}'.format(os.path.join(idir, '*.sample')))
+            os.system('rm -rf {}'.format(os.path.join(idir, '*.haps')))
+            
+        # compress back
+        logging.info('compressing back...')
+        ifiles = [os.path.join(idir, u) for u in os.listdir(idir) if (('.ms' in u) or ('.msOut' in u))]
+        for ifile in ifiles:
+            if '.gz' not in ifile:
+                os.system('gzip {}'.format(ifile))
 
 if __name__ == '__main__':
     main()
