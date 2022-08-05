5bbb6bbf94b11f9ecd795fb685c284db2c450544
Dylan Ray <ddray@email.unc.edu>
2022-08-04 22:04:34 -0400

    debugging

--- src/SLURM/format_relate_distributed.py
+++ src/SLURM/format_relate_distributed.py
@@ -37,13 +37,16 @@ def main():
     
     cmd = 'sbatch --mem=16G -t 1-00:00:00 --wrap "python3 src/data/format_relate.py --idir {0} --ms_dir {1} --ofile {2}"'
     idirs = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if not '.' in u])
-    ms_dirs = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if not '.' in u])
     
     
-    for ix in range(1):
-        cmd_ = cmd.format(idirs[ix], ms_dirs[ix], os.path.join(args.odir, '{0:04d}.hdf5'.format(ix)))
-        print(cmd_)
-        os.system(cmd_)
+    
+    for ix in range(len(idirs)):
+        ms_dir = os.path.join(args.ms_dir, idirs[ix].split('/')[-1])
+        
+        if os.path.exists(ms_dir):
+            cmd_ = cmd.format(idirs[ix], ms_dir, os.path.join(args.odir, '{0:04d}.hdf5'.format(ix)))
+            print(cmd_)
+            os.system(cmd_)
         
     # ${code_blocks}
 
