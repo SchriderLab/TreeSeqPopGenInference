a6324c112dbae1cfcd7dfa45e896bc729fe2b46b
Dylan Ray <ddray@email.unc.edu>
2022-08-22 03:49:11 -0400

    stuff

--- src/data/format_relate.py
+++ src/data/format_relate.py
@@ -65,7 +65,8 @@ def main():
         anc_files = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if (u.split('.')[-1] == 'anc' and tag in u)])
         if len(anc_files) == 0:
             logging.info('ERROR: have no matching .anc files for {}...'.format(ifile))
-        
+        else:
+            logging.info('have {} anc files...')
         indices_f = list(range(len(anc_files)))
         random.shuffle(indices_f)
         
