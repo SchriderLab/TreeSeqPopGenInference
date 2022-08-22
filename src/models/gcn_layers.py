522406cdfcb1a3b140cf4027230b0cfb27560143
Dylan Ray <ddray@email.unc.edu>
2022-08-22 14:06:44 -0400

    trying a simpler model + more seq length

--- src/models/gcn_layers.py
+++ src/models/gcn_layers.py
@@ -746,7 +746,7 @@ class MLP(nn.Module):
     
 class GATSeqClassifier(nn.Module):
     def __init__(self, n_classes = 3, in_dim = 6, gcn_dim = 26, n_gcn_layers = 4, 
-                             num_gru_layers = 1, hidden_size = 256, L = 32, n_heads = 1, n_gcn_iter = 8):
+                             num_gru_layers = 1, hidden_size = 256, L = 32, n_heads = 1, n_gcn_iter = 6):
         super(GATSeqClassifier, self).__init__()
 
         self.gcns = nn.ModuleList()
