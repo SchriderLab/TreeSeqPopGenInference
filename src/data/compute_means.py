d32cff8e5e0f0c5d26c89fe2e844a2ab5ee0f8e2
Dylan Ray <ddray@email.unc.edu>
2022-08-25 15:01:11 -0400

    stuff

--- src/data/compute_means.py
+++ src/data/compute_means.py
@@ -54,7 +54,7 @@ def main():
                 
                 _.extend(x)
             
-        print(np.mean(_), np.std(_), np.max(_), np.min(_), np.mean(ls), np.min(ls))
+        print(np.mean(_), np.std(_), np.max(_), np.min(_), np.mean(ls), np.min(ls), np.max(ls))
 
     # ${code_blocks}
 
