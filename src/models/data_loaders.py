fixed normalization in data loader

src/models/data_loaders.py
@@ -42,8 +42,8 @@ class TreeSeqGeneratorV2(object):
             self.ix += 1
             
             x = np.array(self.ifile[key]['x'])
-            ii = np.where(x[:,:,0] > 0) 
-            x[ii[0],ii[1],0] = np.log(x[ii[0],ii[1],0])
+            ii = np.where(x[:,:,:,0] > 0) 
+            x[ii[0],ii[1],ii[2],0] = np.log(x[ii[0],ii[1],ii[2],0])
             
             y_ = np.array(self.ifile[key]['y'])
             x1 = np.array(self.ifile[key]['x1'])