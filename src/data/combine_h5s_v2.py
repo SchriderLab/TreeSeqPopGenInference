f3e94d8f155cd7df2978a45d118ebfe6e6946a1b
Dylan Ray <ddray@email.unc.edu>
2022-09-19 20:28:47 -0400

    new combine script

--- src/data/combine_h5s_v2.py
+++ src/data/combine_h5s_v2.py
@@ -141,9 +141,6 @@ def main():
             
             classes_ = [classes[u] for u in y]
             
-            x = to_dense_batch(x, bl)
-            edge_index = unbatch_edge_index(edge_index, bl)
-            
             print(x.shape)
             print(edge_index.shape)
             sys.exit()
