3fe4a4370f0e312e18ae07dcfc0ecf504a9db8c1
Dylan Ray <ddray@email.unc.edu>
2022-09-25 18:19:34 -0400

    stuff

--- src/models/data_loaders.py
+++ src/models/data_loaders.py
@@ -166,7 +166,7 @@ class TreeSeqGenerator(object):
 
                     if sample_mode == 'equi':
                         tree_bins = [0.] + list(np.cumsum(X1_[:,-2]))
-                        print(tree_bins)
+                        #print(tree_bins)
                         ii = np.random.uniform(0., max(tree_bins), self.s_length)
                         
                         ii = sorted(list(np.digitize(ii, tree_bins) - 1))
