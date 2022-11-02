8a1a7ab5b970ab20cd5553c51b29efd8ade83e40
Dylan Ray <ddray@email.unc.edu>
2022-11-02 18:25:59 -0400

    stuff

--- src/models/project_images.py
+++ src/models/project_images.py
@@ -53,6 +53,14 @@ def noise_normalize_(noises):
 
         noise.data.add_(-mean).div_(std)
 
+def even_chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    _ = []
+    
+    for i in range(0, len(lst), n):
+        _.append(lst[i:i + n])
+
+    return _
 
 def get_lr(t, initial_lr, rampdown=0.25, rampup=0.05):
     lr_ramp = min(1, (1 - t) / rampdown)
@@ -95,21 +103,20 @@ def parse_args():
     parser.add_argument("--size", default = "256")
     parser.add_argument("--classes", default = "hard,hard-near,neutral,soft,soft-near")
     
-    parser.add_argument("--ifiles", default = "None")
+    parser.add_argument("--idir", default = "None")
     parser.add_argument("--e_tol", default = "0.008")
     parser.add_argument("--max_step", default = "3000")
     
     parser.add_argument("--ckpt", default = "None")
     
-    parser.add_argument("--batch_size", default = "256")
+    parser.add_argument("--batch_size", default = "32")
     parser.add_argument("--lr", default = "0.1")
     parser.add_argument("--n_chunks", default = "10")
     
     parser.add_argument("--step", type=int, default=1000, help="optimize iterations")
     parser.add_argument("--device", default = "cuda")
 
-    parser.add_argument("--odir", default = "None")
-    parser.add_argument("--ofile", default = "/pine/scr/d/d/ddray/test_latent.npz")
+    parser.add_argument("--ofile", default = "None")
     args = parser.parse_args()
 
     if args.verbose:
@@ -118,10 +125,6 @@ def parse_args():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    if args.odir != "None":
-        if not os.path.exists(args.odir):
-            os.system('mkdir -p {}'.format(args.odir))
-            logging.debug('root: made output directory {0}'.format(args.odir))
     # ${odir_del_block}
 
     return args
@@ -129,7 +132,7 @@ def parse_args():
 def main():
     args = parse_args()
     
-    resize = min(int(args.size), 256)
+    resize = int(args.size)
     device = torch.device(args.device)
     
     g_ema = Generator(int(args.size), 512, 8)
@@ -146,94 +149,108 @@ def main():
         latent_mean = latent_out.mean(0)
         latent_std = ((latent_out - latent_mean).pow(2).sum() / n_mean_latent) ** 0.5
     
-    ifiles = args.ifiles.split(',')
-    max_step = int(args.max_step)
-    e_tol = float(args.e_tol)
+    ifiles = sorted(glob.glob(os.path.join(args.idir, '*.png')))
     
-    classes = args.classes.split(',')
-
-    transform = transforms.Compose(
-        [
-            transforms.Resize(resize),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-        ]
-    )
+    ifile_chunks = even_chunks(ifiles, int(args.batch_size))
     
-    result = dict()
-    
-    imgs = []
     y = []
+    noises = dict()
+    latent = []
     
-    for imgfile in ifiles:
-        img = transform(Image.open(imgfile).convert("RGB"))
-        imgs.append(img)
+    for ix in range(len(ifile_chunks)):
+        logging.info('working on chunk {} of {}...'.format(ix + 1, len(ifile_chunks)))
+        ifiles = ifile_chunks[ix]
         
-        c = imgfile.split('/')[-2]
-        y.append(classes.index(c))
-
-    imgs = torch.stack(imgs, 0).to(device)
+        max_step = int(args.max_step)
+        e_tol = float(args.e_tol)
+        
+        classes = args.classes.split(',')
     
-    noises_single = g_ema.make_noise()
-    noises = []
-    for noise in noises_single:
-        noises.append(noise.repeat(imgs.shape[0], 1, 1, 1).normal_())
-
-    latent_in = latent_mean.detach().clone().unsqueeze(0).repeat(imgs.shape[0], 1)
-    latent_in.requires_grad = True
-    
-    for noise in noises:
-        noise.requires_grad = True
-
-    optimizer = optim.Adam([latent_in] + noises, lr=float(args.lr))
-    
-    #pbar = tqdm(range(args.step))
-    latent_path = []
-
-    for i in range(max_step):
-        t = i / args.step
-        lr = get_lr(t, float(args.lr))
-        optimizer.param_groups[0]["lr"] = lr
-        noise_strength = latent_std * 0.05 * max(0, 1 - t / 0.75) ** 2
-        latent_n = latent_noise(latent_in, noise_strength.item())
-
-        img_gen, _ = g_ema([latent_n], input_is_latent=True, noise=noises)
-
-        batch, channel, height, width = img_gen.shape
-
-        if height > 256:
-            factor = height // 256
-
-            img_gen = img_gen.reshape(
-                batch, channel, height // factor, factor, width // factor, factor
-            )
-            img_gen = img_gen.mean([3, 5])
-
-        #p_loss = percept(img_gen, imgs).sum()
-        n_loss = noise_regularize(noises)
-        mse_loss = F.mse_loss(img_gen, imgs)
-
-        loss = mse_loss + 1e-5 * n_loss
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        noise_normalize_(noises)
-
-        if (i + 1) % 100 == 0:
-            latent_path.append(latent_in.detach().clone())
+        transform = transforms.Compose(
+            [
+                transforms.Resize(resize),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ]
+        )
+        
+        imgs = []
+        for imgfile in ifiles:
+            img = transform(Image.open(imgfile).convert("RGB"))
+            imgs.append(img)
             
-            logging.info('step {}: mse loss {}...'.format(i + 1, mse_loss.item()))
-
-        if mse_loss.item() < e_tol:
-            break
+            c = imgfile.split('/')[-2]
+            y.append(classes.index(c))
     
-    latent_in = latent_in.detach().cpu().numpy()
-    noises = dict(zip(list(map(str, list(range(len(noises))))), [u.detach().cpu().numpy() for u in noises]))
+        imgs = torch.stack(imgs, 0).to(device)
+        
+        noises_single = g_ema.make_noise()
+        noises = []
+        for noise in noises_single:
+            noises.append(noise.repeat(imgs.shape[0], 1, 1, 1).normal_())
+    
+        latent_in = latent_mean.detach().clone().unsqueeze(0).repeat(imgs.shape[0], 1)
+        latent_in.requires_grad = True
+        
+        for noise in noises:
+            noise.requires_grad = True
+    
+        optimizer = optim.Adam([latent_in] + noises, lr=float(args.lr))
+    
+        for i in range(max_step):
+            t = i / args.step
+            lr = get_lr(t, float(args.lr))
+            optimizer.param_groups[0]["lr"] = lr
+            noise_strength = latent_std * 0.05 * max(0, 1 - t / 0.75) ** 2
+            latent_n = latent_noise(latent_in, noise_strength.item())
+    
+            img_gen, _ = g_ema([latent_n], input_is_latent=True, noise=noises)
+    
+            batch, channel, height, width = img_gen.shape
+    
+            if height > 256:
+                factor = height // 256
+    
+                img_gen = img_gen.reshape(
+                    batch, channel, height // factor, factor, width // factor, factor
+                )
+                img_gen = img_gen.mean([3, 5])
+    
+            #p_loss = percept(img_gen, imgs).sum()
+            n_loss = noise_regularize(noises)
+            mse_loss = F.mse_loss(img_gen, imgs)
+    
+            loss = mse_loss + 1e-5 * n_loss
+    
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    
+            noise_normalize_(noises)
+    
+            if (i + 1) % 100 == 0:
+                logging.info('step {}: mse loss {}...'.format(i + 1, mse_loss.item()))
+    
+            if mse_loss.item() < e_tol:
+                break
+        
+        latent_in = latent_in.detach().cpu().numpy()
+        for k in range(len(noises)):
+            if str(k) not in noises.keys():
+                noises[str(k)] = [noises[k].detach().cpu().numpy()]
+            else:
+                noises[str(k)].append(noises[k].detach().cpu().numpy())
+                
+        latent.append(latent_in)
+        
+        noises = dict(zip(list(map(str, list(range(len(noises))))), [u.detach().cpu().numpy() for u in noises]))
+    
+    latent = np.concatenate(latent, 0)
+    for k in noises.keys():
+        noises[k] = np.concatenate(noises[k], 0)
     
     logging.info('saving...')
-    np.savez(args.ofile, latent = latent_in, y = np.array(y, dtype = np.int32), **noises)
+    np.savez(args.ofile, latent = latent, y = np.array(y, dtype = np.int32), **noises)
     # ${code_blocks}
 
 if __name__ == '__main__':
