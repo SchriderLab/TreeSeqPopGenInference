5a722e4afe1d4dcc8b1c38292fafeb1b1c651d96
Dylan Ray <ddray@email.unc.edu>
2023-02-01 19:12:20 -0500

    bug fix

--- src/models/compute_kl_matrix_vectorized.py
+++ src/models/compute_kl_matrix_vectorized.py
@@ -57,10 +57,10 @@ def F_mig(alpha, m, N_div, t):
     ret = np.zeros(alpha.shape)
     i, j, k = np.where(alpha != 0)
     
-    ret[i, j, k] = 1 - np.exp(m[i, j, k] * (N_div[i, j, k] ** -1) * (np.exp(t[:,:,k] * alpha[i, j, k]) - 1) / (-1 * alpha[i, j, k]))
+    ret[i, j, k] = 1 - np.exp(m[i, j, k] * (N_div[i, j, k] ** -1) * (np.exp(t[k,:,:] * alpha[i, j, k]) - 1) / (-1 * alpha[i, j, k]))
     i, j, k = np.where(alpha == 0)
     
-    ret[i, j, k] = 1 - np.exp(-m[i, j, k] * t[:,:,k])
+    ret[i, j, k] = 1 - np.exp(-m[i, j, k] * t[k,:,:])
     
     return ret
 
@@ -158,7 +158,7 @@ def compute_P(events, N, alpha, m):
     P_coal = np.log2(P_coal) * s
     
     P_coal[ii_coal,ii_pop] = P_coal[ii_coal, ii_pop] / s[ii_coal, ii_pop]
-    P_coal[ii_coal,ii_pop] += np.log2(1 - 2 ** np.exp(P_coal[ii_coal,ii_pop])) * (s[ii_coal, ii_pop] - 1)
+    P_coal[ii_coal,ii_pop] += np.log2(1 - 2 ** P_coal[ii_coal,ii_pop]) * (s[ii_coal, ii_pop] - 1)
 
     #p = np.sum(np.log2(_) * s, axis = 1)
     
