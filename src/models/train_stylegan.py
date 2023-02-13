137966f3aa72c40bc25a5e610ca053e7c62bb26f
Dylan Ray <ddray@email.unc.edu>
2023-02-12 22:07:55 -0500

    stuff

--- src/models/train_stylegan.py
+++ src/models/train_stylegan.py
@@ -224,7 +224,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             break
 
         real_img = next(loader)
-        print(type(real_img[0]), real_img[0].shape)
+        print(type(real_img[0]), real_img[0].shape, len(real_img))
         
         real_img = (real_img.to(device).to(torch.float32) / 127.5 - 1)
 
