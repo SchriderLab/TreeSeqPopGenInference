2bcb7b3f18f06dc5f1cf4ba8b38afd113cb9df22
Dylan Ray <ddray@email.unc.edu>
2023-02-13 17:14:22 -0500

    added more stats

--- src/data/coal_time_statistics.py
+++ src/data/coal_time_statistics.py
@@ -53,17 +53,32 @@ def main():
     maxs = []
     mins = []
     
+    l = []
     for ifile in ifiles:
         x = np.load(ifile)
         
         D = x['D']
+        loc = x['loc']
+        l.append(loc[-1])
+        
         Dmax = np.max(np.log(D))
         Dmin = np.min(np.log(D))
 
         maxs.append(Dmax)
         mins.append(Dmin)
+        
+        mean.append(np.mean(Dmax))
+        var.append(np.std(Dmax))
+        
+    plt.scatter(mean, var, c = l, cmap = 'viridis')
+    plt.savefig('mean_std_a001.png', dpi = 100)
+    plt.close()
 
     print(np.max(maxs))
+    print(np.mean(maxs))
+    print(np.std(maxs))
+    print(np.mean(mins))
+    print(np.std(mins))
     print(np.min(mins))
 
     # ${code_blocks}
