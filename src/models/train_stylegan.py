bd92b07434e00387cdfb98a8cf2cc7a43f3ac21b
Dylan Ray <ddray@email.unc.edu>
2023-02-23 19:36:59 -0500

    stuff

--- src/models/train_stylegan.py
+++ src/models/train_stylegan.py
@@ -401,7 +401,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                     }
                 )
 
-            if i % 100 == 0:
+            if i % 500 == 0:
                 with torch.no_grad():
                     g_ema.eval()
                     sample = g_ema(sample_z)
