fbbccc659ad02750ad1122f337b938e25ae1f01a
Dylan Ray <ddray@email.unc.edu>
2023-03-03 17:31:11 -0500

    added fast distance to the Relate routines

--- src/data/format_relate_v2.py
+++ src/data/format_relate_v2.py
@@ -202,6 +202,9 @@ def main():
                         _.extend(c.children)
                     
                     children = copy.copy(_)
+                    
+                print(children)
+                print(root.count())
                 
                 D = make_distance_matrix(root, sample_sizes)
                 Ds.append(squareform(D))
