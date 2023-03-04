82a267e5f1d127b143b1228b1ea24f9ed31f68be
Dylan Ray <ddray@email.unc.edu>
2023-03-04 18:22:20 -0500

    stuff

--- src/SLURM/format_relate_distributed.py
+++ src/SLURM/format_relate_distributed.py
@@ -41,21 +41,15 @@ def parse_args():
 def main():
     args = parse_args()
     
-    cmd = 'sbatch --mem=16G -t 2-00:00:00 -o {3} --wrap "python3 src/data/format_relate_v2.py --idir {0} --ms_dir {1} --ofile {2} --pop_sizes {4} --n_sample {5} --topological_order"'
+    cmd = 'sbatch --mem=16G -t 2-00:00:00 -o {3} --wrap "python3 src/data/format_relate_discoal.py --idir {0} --ofile {1} --pop_sizes {2}"'
     idirs = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if not '.' in u])
-    
-    if not args.topological_order:
-        cmd = cmd.replace(' --topological_order', '')
-    
-    for ix in range(len(idirs)):
-        ms_dir = os.path.join(args.ms_dir, idirs[ix].split('/')[-1])
         
-        if os.path.exists(ms_dir):
-            cmd_ = cmd.format(idirs[ix], ms_dir, os.path.join(args.odir, '{0:04d}.hdf5'.format(ix)), os.path.join(args.odir, '{0:04d}_slurm.out'.format(ix)), args.pop_sizes, args.n_sample)
-            
-            print(cmd_)
-            if not args.only_print:
-                os.system(cmd_)
+    for ix in range(len(idirs)):        
+        cmd_ = cmd.format(idirs[ix], args.pop_sizes, os.path.join(args.odir, '{0:04d}.hdf5'.format(ix)), os.path.join(args.odir, '{0:04d}_slurm.out'.format(ix)))
+        
+        print(cmd_)
+        if not args.only_print:
+            os.system(cmd_)
         
     # ${code_blocks}
 
