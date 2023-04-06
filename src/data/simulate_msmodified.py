0e99cc52fc2cc60710fca66e34986d422c7e4624
Dylan Ray <ddray@email.unc.edu>
2023-04-06 18:33:46 -0400

    stuff

--- src/data/simulate_msmodified.py
+++ src/data/simulate_msmodified.py
@@ -9,6 +9,8 @@ from data_functions import writeTbsFile
 import copy
 import subprocess
 
+MSMOD_PATH = 'msdir/ms'
+
 # this function creates an array for writing to text that has the ms parameters
 # from a CSV file produced via bootstrapped DADI runs
 # the values expected in the CSV our given below
@@ -48,7 +50,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # my args
     parser.add_argument("--verbose", action = "store_true", help = "display messages")
-    parser.add_argument("--n_samples", default = "2048", help = "number of alignments to simulate per job")
+    parser.add_argument("--n_samples", default = "1000", help = "number of alignments to simulate per job")
     parser.add_argument("--n_jobs", default = "1", help = "number of jobs.  If your on a system without SLURM, then this should be left to 1 (default)")
     
     parser.add_argument("--ifile", default = "params.txt", help = "CSV file of bootstrapped demographic estimates. only applicable for the drosophila case; --model dros")
@@ -57,12 +59,10 @@ def parse_args():
     parser.add_argument("--direction", default = "ab", help = "directionality of migration. only applicable for the drosophila case; --model dros")
     parser.add_argument("--slurm", action = "store_true")
     
-    parser.add_argument("--window_size", default = "5000", help = "size in base pairs of the region to simulate")
+    parser.add_argument("--window_size", default = "50000", help = "size in base pairs of the region to simulate")
     
     parser.add_argument("--mt_range", default = "None")
     parser.add_argument("--t_range", default = "None")
-    
-    parser.add_argument("--n_grid_points", default = "5")
     
     parser.add_argument("--trees", action = "store_true")
 
@@ -83,8 +83,6 @@ def parse_args():
 
     return args
 
-import itertools
-
 def main():
     args = parse_args()
     
@@ -103,84 +101,96 @@ def main():
     
     counter = 0
     
-    migTime = np.linspace(1e-3, 0.75, 6)
-    migProb = np.linspace(0., 0.99, 6)
-    rho = np.linspace(0.1, 0.3, 6)
-    T = np.linspace(0.4, 1.7, 6)
-    
-    params = list(itertools.product(migTime, migProb, rho, T))
-    
-    df = df[np.where(df[:,0] > -2000)[0],:]
-    df_mean = np.mean(df, axis = 0).reshape(1, -1)
-    
-    # size of the populations
-    SIZE_A = 64
-    SIZE_B = 64
-    
-    P, ll, Nref = parameters_df(df_mean, 0, 1., 0., 0., n)
-    for p in params:
-        migTime, migProb, rho, T = p
+    for j in range(int(args.n_jobs)):
+        if args.model == 'dros':
+            for ix in range(df.shape[0]):
+                # size of the populations
+                SIZE_A = 20
+                SIZE_B = 14
+                
+                P, ll, Nref = parameters_df(df, ix, 1., 0., 0., n)
+                if ll > -2000:
+                    odir = os.path.join(args.odir, 'iter{0:06d}'.format(counter))
+                    counter += 1
+                    
+                    os.system('mkdir -p {}'.format(odir))
+                    
+                    if args.mt_range != "None":
+                        mt_range = tuple(list(map(float, args.mt_range.split(','))))
+                        
+                        # replace mean migTime and the rest with a uniformly random distribution around it
+                        migTime = np.random.uniform(mt_range[0], mt_range[1], (P.shape[0], ))
+                    else:
+                        migTime = np.random.uniform(0., 0.1, (P.shape[0], ))
+                    
+                    migProb = 1 - np.random.uniform(0., 1.0, (P.shape[0], ))
+                    rho = np.random.uniform(0.1, 0.3, (P.shape[0], ))
+                    
+                    if args.t_range != "None":
+                        t_range = tuple(list(map(float, args.t_range.split(','))))
+                        T = copy.copy(P[:,-4])
+                        
+                        # rescale alpha1 and alpha2
+                        P[:,4] *= T
+                        P[:,5] *= T
+                        
+                        P[:,-4] = np.random.uniform(t_range[0], t_range[1], (P.shape[0], ))
+                        P[:,-4] /= (4*Nref / 15.)
+                        
+                        # rescale alpha1 and alpha2
+                        P[:,4] /= P[:,-4]
+                        P[:,5] /= P[:,-4]
+                    
+                    P[:,-1] = migTime * P[:,-4]
+                    P[:,-3] = migTime * P[:,-4]
+                    
+                    P[:, 1] = P[:,0] / rho
+                    P[:,-2] = migProb
+                
+                    if ('ba' in args.direction) or ('ab' in args.direction):
+                        writeTbsFile(np.concatenate([P, np.random.randint(0, 2**14, size = (P.shape[0], 3))], axis = 1), os.path.join(odir, 'mig.tbs'))
+                    else:
+                        writeTbsFile(np.concatenate([P[:,:-3], np.random.randint(0, 2**14, size = (P.shape[0], 1))], axis = 1), os.path.join(odir, 'mig.tbs'))
+                        
+                    # direction + wtrees (if you desire the output as a series of genealogical trees)
+                    # in the case of genealogical trees, we use regular ms as we observed an error in msmodified when trying...
+                    if not args.trees:
+                        if args.direction == 'ab':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 2 tbs -ej tbs 3 1 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                        elif args.direction == 'ba':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 1 tbs -ej tbs 3 2 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                        elif args.direction == 'none':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -seed tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                    else:
+                        if args.direction == 'ab':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -T -L -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 2 tbs -ej tbs 3 1 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                        elif args.direction == 'ba':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -T -L -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 1 tbs -ej tbs 3 2 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                        elif args.direction == 'none':
+                            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -T -L -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -seed tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), MSMOD_PATH), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
+                    
+                    # example command:
+                    # 34 1 -t 58.3288 -r 365.8836 10000 -T -L -I 2 20 14 -n 1 18.8855 -n 2 0.05542 -eg 0 1 6.5160 -eg 0 2 -7.5960 -ma x 0.0 0.0 x -ej 0.66698 2 1 -en 0.66698 1 1 -es 0.02080 2 0.343619 -ej 0.02080 3 1 -seeds 12674 8050 3617
+                    
+                    # print the command, do the command, gzip the outputs
+                    cmd = "echo '{0}' && {0} && gzip {1}.mig.msOut".format(cmd, direction)
+                    print('simulating for parameters: {}'.format(P))
+                    sys.stdout.flush()
+                    
+                    if args.slurm:
+                        fout = os.path.join(odir, 'slurm.out')
+                        cmd = slurm_cmd.format(fout, cmd)
+                        
+                        print(cmd)
+                        os.system(cmd)
+                    else:
+                        print(cmd)
+                        
+                        # New process, connected to the Python interpreter through pipes:
+                        prog = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        prog.communicate()
         
-        odir = os.path.join(args.odir, 'iter{0:06d}'.format(counter))
-        counter += 1
-        
-        os.system('mkdir -p {}'.format(odir))
-        migProb = 1 - migProb
-        
-        #t_range = tuple(list(map(float, args.t_range.split(','))))
-        T_ = copy.copy(P[:,-4])
-        
-        # rescale alpha1 and alpha2
-        P[:,4] *= T_
-        P[:,5] *= T_
-        
-        P[:,-4] = T
-        P[:,-5] = T
-        #P[:,-4] /= (4*Nref / 15.)
-        
-        # rescale alpha1 and alpha2
-        P[:,4] /= P[:,-4]
-        P[:,5] /= P[:,-4]
-        
-        P[:,-1] = migTime * P[:,-4]
-        P[:,-3] = migTime * P[:,-4]
-        
-        P[:, 1] = P[:,0] / rho
-        P[:,-2] = migProb
-    
-        if ('ba' in args.direction) or ('ab' in args.direction):
-            writeTbsFile(np.concatenate([P, np.random.randint(0, 2**14, size = (P.shape[0], 3))], axis = 1), os.path.join(odir, 'mig.tbs'))
-        else:
-            writeTbsFile(np.concatenate([P[:,:-3], np.random.randint(0, 2**14, size = (P.shape[0], 1))], axis = 1), os.path.join(odir, 'mig.tbs'))
             
-        
-        if args.direction == 'ab':
-            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 2 tbs -ej tbs 3 1 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), 'msdir/ms'), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
-        elif args.direction == 'ba':
-            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -es tbs 1 tbs -ej tbs 3 2 -seeds tbs tbs tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), 'msdir/ms'), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
-        elif args.direction == 'none':
-            cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -seed tbs < %s | tee %s" % (odir, os.path.join(os.getcwd(), 'msdir/ms'), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
-        
-        # example command:
-        # 34 1 -t 58.3288 -r 365.8836 10000 -T -L -I 2 20 14 -n 1 18.8855 -n 2 0.05542 -eg 0 1 6.5160 -eg 0 2 -7.5960 -ma x 0.0 0.0 x -ej 0.66698 2 1 -en 0.66698 1 1 -es 0.02080 2 0.343619 -ej 0.02080 3 1 -seeds 12674 8050 3617
-        
-        # print the command, do the command, gzip the outputs
-        cmd = "echo '{0}' && {0} && gzip {1}.mig.msOut".format(cmd, args.direction)
-        print('simulating for parameters: {}'.format(P[0]))
-        sys.stdout.flush()
-        
-        print(cmd)
-        
-        """
-        fout = os.path.join(odir, 'slurm.out')
-        cmd = slurm_cmd.format(fout, cmd)
-        
-        print(cmd)
-        os.system(cmd)
-        """
-        
-        
-               
 if __name__ == '__main__':
     main()
 
