22a6ac75e9c412a933c28f9ca573eefd46b4cae6
Dylan Ray <ddray@email.unc.edu>
2023-04-11 20:12:44 -0400

    added script for geno matrix formatting

--- src/data/combine_h5s_genomat.py
+++ src/data/combine_h5s_genomat.py
@@ -13,58 +13,72 @@ import h5py
 from mpi4py import MPI
 from collections import deque
 
-def seriate_match(x, pop_sizes = (20, 14), out_shape = (2, 32, 128), metric = 'cosine'):
+def format_matrix(x, pop_sizes = (20, 14), out_shape = (2, 32, 128), metric = 'cosine', mode = 'seriate_match'):
     s0, s1 = pop_sizes
     n_pops, n_ind, n_sites = out_shape
     
     if x.shape[0] != s0 + s1:
         return None
     
-    x0 = x[:s0,:]
-    x1 = x[s0:s0 + s1,:]
-    
-    if s0 != n_ind:
-        ii = np.random.choice(range(s0), n_ind)
-        x0 = x0[ii,:]
-    
-    if s1 != n_ind:
-        ii = np.random.choice(range(s1), n_ind)
-        x1 = x1[ii,:]
-
-    if x0.shape[1] > n_sites:
-        ii = np.random.choice(range(x0.shape[1] - n_sites))
+    if mode == 'seriate_match':
+        x0 = x[:s0,:]
+        x1 = x[s0:s0 + s1,:]
         
-        x0 = x0[:,ii:ii + n_sites]
-        x1 = x1[:,ii:ii + n_sites]
+        if s0 != n_ind:
+            ii = np.random.choice(range(s0), n_ind)
+            x0 = x0[ii,:]
         
-    else:
-        to_pad = n_sites - x0.shape[1]
+        if s1 != n_ind:
+            ii = np.random.choice(range(s1), n_ind)
+            x1 = x1[ii,:]
     
-        if to_pad % 2 == 0:
-            x0 = np.pad(x0, ((0,0), (to_pad // 2), (to_pad // 2)))
-            x1 = np.pad(x1, ((0,0), (to_pad // 2), (to_pad // 2)))
+        if x0.shape[1] > n_sites:
+            ii = np.random.choice(range(x0.shape[1] - n_sites))
+            
+            x0 = x0[:,ii:ii + n_sites]
+            x1 = x1[:,ii:ii + n_sites]
+            
         else:
-            x0 = np.pad(x0, ((0,0), (to_pad // 2 + 1), (to_pad // 2)))
-            x1 = np.pad(x1, ((0,0), (to_pad // 2 + 1), (to_pad // 2)))
+            to_pad = n_sites - x0.shape[1]
+        
+            if to_pad % 2 == 0:
+                x0 = np.pad(x0, ((0,0), (to_pad // 2), (to_pad // 2)))
+                x1 = np.pad(x1, ((0,0), (to_pad // 2), (to_pad // 2)))
+            else:
+                x0 = np.pad(x0, ((0,0), (to_pad // 2 + 1), (to_pad // 2)))
+                x1 = np.pad(x1, ((0,0), (to_pad // 2 + 1), (to_pad // 2)))
+    
+        # seriate population 1
+        D = squareform(pdist(x0, metric = metric))
+        D[np.isnan(D)] = 0.
+        
+        ii = seriate(D, timeout = 0.)
+        
+        x0 = x0[ii]
+        
+        D = cdist(x0, x1, metric = metric)
+        D[np.isnan(D)] = 0.
+        
+        i, j = linear_sum_assignment(D)
+        
+        x1 = x1[j]
+        
+        return np.concatenate([np.expand_dims(x0, 0), np.expand_dims(x1, 0)], 0)
+    elif mode == 'pad':
+        if x.shape[1] > n_sites:
+            ii = np.random.choice(range(x.shape[1] - n_sites))
+            
+            x = x[:,ii:ii + n_sites]
+        else:
+            to_pad = n_sites - x.shape[1]
 
-    # seriate population 1
-    D = squareform(pdist(x0, metric = metric))
-    D[np.isnan(D)] = 0.
+            if to_pad % 2 == 0:
+                x = np.pad(x, ((0,0), (to_pad // 2), (to_pad // 2)))
+            else:
+                x = np.pad(x, ((0,0), (to_pad // 2 + 1), (to_pad // 2)))
     
-    ii = seriate(D, timeout = 0.)
+        return x
     
-    x0 = x0[ii]
-    
-    D = cdist(x0, x1, metric = metric)
-    D[np.isnan(D)] = 0.
-    
-    i, j = linear_sum_assignment(D)
-    
-    x1 = x1[j]
-    
-    return np.concatenate([np.expand_dims(x0, 0), np.expand_dims(x1, 0)], 0)
-    
-
 # use this format to tell the parsers
 # where to insert certain parts of the script
 # ${imports}
