5f33352e5e5427ca4ae84b3d5e297448e1805686
Dylan Ray <ddray@email.unc.edu>
2023-05-08 14:36:43 -0400

    debugging

--- src/data/format_genomat.py
+++ src/data/format_genomat.py
@@ -199,8 +199,7 @@ def main():
             X_ = []
             P_ = []
             for ix, x in enumerate(X):
-                
-                print(x.shape)
+                logging.info('have shape of {}...'.format(x.shape))
                 x, p = format_matrix(x, P[ix], pop_sizes, out_shape = tuple(map(int, args.out_shape.split(','))), mode = args.mode)
                 
                 if x is not None:
