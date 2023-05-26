dd0bccf093858a4f83d3575c791c7356a5495152
Dylan Ray <ddray@email.unc.edu>
2023-05-26 12:30:16 -0400

    debugging

--- src/data/compute_means.py
+++ src/data/compute_means.py
@@ -58,8 +58,9 @@ def main():
             mask = np.array(ifile[key]['mask'])
 
             if args.regression:
-                ys.append(np.log(np.array(ifile[key]['y'])[0,0].flatten()))
-
+                ys.append(np.log(np.array(ifile[key]['y'])))
+                print(ys[-1].shape)
+                
             ls.append(x.shape[0])
             
             x_ = x[:,:,:,0]
