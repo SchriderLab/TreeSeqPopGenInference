f0cc6a3b89847c12793b9036980a592b204af130
Dylan Ray <ddray@email.unc.edu>
2023-07-06 13:02:30 -0400

    testing data loading routines

--- src/data/data_loading_sanity.py
+++ src/data/data_loading_sanity.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 
 from data_functions import load_data
-from format_genomat import find_files
+from format_genomat import find_files, format_matrix
 
 # use this format to tell the parsers
 # where to insert certain parts of the script
@@ -17,6 +17,10 @@ def parse_args():
     parser.add_argument("--verbose", action = "store_true", help = "display messages")
     parser.add_argument("--idir", default = "/overflow/dschridelab/ddray/four_problems/dros/mt2_4/eval_sims_i2/")
     parser.add_argument("--n_sample", default = "34")
+    
+    parser.add_argument("--pop_sizes", default = "20,14")
+    parser.add_argument("--chunk_size", default = "4")
+    parser.add_argument("--out_shape", default = "1,34,512")
 
     parser.add_argument("--odir", default = "None")
     args = parser.parse_args()
@@ -41,6 +45,8 @@ def main():
     ifiles = []
     
     classes = sorted(os.listdir(args.idir))
+    pop_sizes = list(map(int, args.pop_sizes.split(',')))
+    chunk_size = int(args.chunk_size)
     
     for c in classes:
         idir = os.path.join(args.idir, c)
@@ -48,15 +54,19 @@ def main():
         
     lengths = []
     for tag, ifile in ifiles:
-        x, y, p, params = load_data(ifile)
+        X, y, P, params = load_data(ifile)
         
-        if any([u is None for u in x]):
+        if any([u is None for u in X]):
             print(ifile)
             
-        if any([u.shape[0] != int(args.n_sample) for u in x]):
+        if any([u.shape[0] != int(args.n_sample) for u in X]):
             print(ifile)
+            
+        for ix, x in enumerate(X):
+            x, p = format_matrix(x, P[ix], pop_sizes, out_shape = tuple(map(int, args.out_shape.split(','))), mode = args.mode)
         
-
+            if x is None:
+                print(ix, ifile)
     # ${code_blocks}
 
 if __name__ == '__main__':
